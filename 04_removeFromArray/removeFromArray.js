327c6a4ef355f3ffa195a1bf822830d8f20e454c
Francesco <caia.fra96@hotmail.it>
2022-05-07 19:58:19 +0200

    removeFromArray.js added comments

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,9 +1,11 @@
 const removeFromArray = function(...args) {
+  // Get the array to check from the arguments list
   const array = args[0];
-
+  // Create a new empty array
   const newArray = [];
-
+  // Go through every item in the array
   array.forEach((item) => {
+    // Push every item into the new array UNLESS it's included in the function arguments
     if (!args.includes(item)) {
       newArray.push(item);
     }
