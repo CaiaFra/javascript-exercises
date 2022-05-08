f0371be2fb0373e1d74d82281275c741913b53e2
Francesco <caia.fra96@hotmail.it>
2022-05-08 12:52:03 +0200

    sumAll.js solved

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,24 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+  if (num1 < 0 || num2 < 0) return 'ERROR';
+  if (typeof num1 !== 'number' || typeof num2 !== 'number') return 'ERROR';
+  
+  let sum = 0;
+  let major, minor;
+
+  if (num1 > num2) {
+    major = num1;
+    minor = num2;
+  } else if (num2 > num1) {
+    major = num2;
+    minor = num1;
+  } else {
+    return num1;
+  }
+
+  for (let i = minor; i <= major; i++) {
+    sum += i;
+  }
+  return sum;
 
 };
 
